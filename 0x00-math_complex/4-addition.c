177a0302f5caca8fa000aa57f94bb0e2975ac3de
oxyvans <benja.1999.g@hotmail.com>
2022-03-18 09:52:12 -0700

    04

---
+++ 0x00-math_complex/4-addition.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * addition - addition.
+ * @c1: complex to add.
+ * @c2: complex to add.
+ * @c3: res
+ */
+
+void addition(complex c1, complex c2, complex *c3)
+{
+	c3->re = c1.re + c2.re;
+	c3->im = c1.im + c2.im;
+}
